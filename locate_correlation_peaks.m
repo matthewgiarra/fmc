20be1768c3598dc6ec09ad0d9a94e837c4093b99
Matthew Giarra <matthew.giarra@gmail.com>
2014-11-17 17:06:23 -0500

    Commented locate_correlation_peaks.m with compiling instructions and simplified compile_locate_correlation_peaks.m

--- locate_correlation_peaks.m
+++ locate_correlation_peaks.m
@@ -4,8 +4,18 @@ function PEAK_MATRIX = locate_correlation_peaks(SPATIAL_CORRELATION)
 % and returns the plane with peaks extracted. 
 % This tiny function is separate from the main subpixel code
 % so that it can be compiled easily.
+% 
+% Compile with: codegen -config coder.config('mex') locate_correlation_peaks -args {coder.typeof(1.00, [inf, inf])};
+%
+% INPUTS:
+%	SPATIAL_CORRELATION = [M x N] matrix containing a spatial correlation plane
+%
+% OUTPUTS:
+%	PEAK_MATRIX = [M x N] matrix containing the locations and magnitudes of the identified peaks.
 
- %Locate peaks using imregionalmax
+
+
+%Locate peaks using imregionalmax
 PEAK_MATRIX = imregionalmax(SPATIAL_CORRELATION) .* SPATIAL_CORRELATION;
 
 end
